0f000cc148ab0593ab26408e7a7be1b24f942154
Guru Senthil <guru.koushik@gmail.com>
2023-09-30 11:56:50 -0700

    fix: format and remove make command for test plan

--- main.cpp
+++ main.cpp
@@ -21,33 +21,38 @@ bool input(int argc, char** argv) {
 
 enum Method {
   /*
-  Find the minimum deviation city which is reachable with a fully charged battery which satisfies
-  minimum(d(start, city) + d(city, goal) - d(start, goal)) and d(start, city) < MAX_RANGE
-  At each city, charge the battery to full capacity.
+  Find the minimum deviation city which is reachable with a fully charged
+  battery which satisfies minimum(d(start, city) + d(city, goal) - d(start,
+  goal)) and d(start, city) < MAX_RANGE At each city, charge the battery to full
+  capacity.
   */
   BRUTE_FORCE_CHARGE_FULL,
   /*
-  Find the minimum deviation city which is reachable with a fully charged battery which satisfies
-  minimum(d(start, city) + d(city, goal) - d(start, goal)) and d(start, city) < MAX_RANGE
-  At each city, charge the battery just enough to reach the next city.
+  Find the minimum deviation city which is reachable with a fully charged
+  battery which satisfies minimum(d(start, city) + d(city, goal) - d(start,
+  goal)) and d(start, city) < MAX_RANGE At each city, charge the battery just
+  enough to reach the next city.
   */
   BRUTE_FORCE_CHARGE_GREEDY,
   /*
-  Find the minimum deviation city which is reachable with a fully charged battery which satisfies
-  minimum(d(start, city) + d(city, goal) - d(start, goal)) and d(start, city) < MAX_RANGE
-  Find the optimal charging strategy which minimizes the sum of time spent charging at each city
+  Find the minimum deviation city which is reachable with a fully charged
+  battery which satisfies minimum(d(start, city) + d(city, goal) - d(start,
+  goal)) and d(start, city) < MAX_RANGE Find the optimal charging strategy which
+  minimizes the sum of time spent charging at each city
   */
   BRUTE_FORCE_CHARGE_OPTIMAL,
   /*
-  Run a Monte Carlo simulation to randomly find different paths from start to goal and evaluate
-  the total time taken to complete the trip. Keep track of the path which takes the minimum time.
-  At each city, charge the battery just enough to reach the next city.
+  Run a Monte Carlo simulation to randomly find different paths from start to
+  goal and evaluate the total time taken to complete the trip. Keep track of the
+  path which takes the minimum time. At each city, charge the battery just
+  enough to reach the next city.
   */
   MONTE_CARLO_CHARGE_GREEDY,
   /*
-  Run a Monte Carlo simulation to randomly find different paths from start to goal and evaluate
-  the total time taken to complete the trip. Keep track of the path which takes the minimum time.
-  Find the optimal charging strategy which minimizes the sum of time spent charging at each city
+  Run a Monte Carlo simulation to randomly find different paths from start to
+  goal and evaluate the total time taken to complete the trip. Keep track of the
+  path which takes the minimum time. Find the optimal charging strategy which
+  minimizes the sum of time spent charging at each city
   */
   MONTE_CARLO_CHARGE_OPTIMAL,
 };
